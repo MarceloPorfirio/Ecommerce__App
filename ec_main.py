1655c2ede47148840e5d93ddf08f5344db116e92
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2024-04-28 06:57:15 -0300

    add

--- ec_main.py
+++ ec_main.py
@@ -35,12 +35,83 @@ class Landing(ft.View):
 
 class Model(object):
     products: dict ={
-        
+       0:{
+            "id": "111",
+            "img_src:": "phone.jpg,",
+            "nome": "Product 1",
+            "description": "Ta procurando um Smartphone com alto desempenho, capaz de executar as tarefas mais comuns do dia a dia com velocidade e eficiência? Então vem conhcer o Moto G54 da Motorola. Para começar, esse smartphone faz parte da linha Moto G, tradicional linha de celulares da Motorola. Ele possui armazenamento interno de 256GB, espaço suficiente para guardar os arquivos como fotos e vídeos. Ah, sem falar que ele conta com tecnologia 5G, Processador Dimensity 7020 e Memória RAM de 8GB, dessa forma você tem uma boa performance em diversas atividades, das mais simples até algumas mais complexas. E esse modelo é na cor Azul. ",
+            "price": "R$ 1200.00",
+       },
+       1:{
+         "id": "112",
+            "img_src:": "phone.jpg,",
+            "nome": "Product 1",
+            "description": "Ta procurando um Smartphone com alto desempenho, capaz de executar as tarefas mais comuns do dia a dia com velocidade e eficiência? Então vem conhcer o Moto G54 da Motorola. Para começar, esse smartphone faz parte da linha Moto G, tradicional linha de celulares da Motorola. Ele possui armazenamento interno de 256GB, espaço suficiente para guardar os arquivos como fotos e vídeos. Ah, sem falar que ele conta com tecnologia 5G, Processador Dimensity 7020 e Memória RAM de 8GB, dessa forma você tem uma boa performance em diversas atividades, das mais simples até algumas mais complexas. E esse modelo é na cor Azul. ",
+            "price": "R$ 1300.00",  
+       } ,
+       2:{
+           "id": "113",
+            "img_src:": "phone.jpg,",
+            "nome": "Product 1",
+            "description": "Ta procurando um Smartphone com alto desempenho, capaz de executar as tarefas mais comuns do dia a dia com velocidade e eficiência? Então vem conhcer o Moto G54 da Motorola. Para começar, esse smartphone faz parte da linha Moto G, tradicional linha de celulares da Motorola. Ele possui armazenamento interno de 256GB, espaço suficiente para guardar os arquivos como fotos e vídeos. Ah, sem falar que ele conta com tecnologia 5G, Processador Dimensity 7020 e Memória RAM de 8GB, dessa forma você tem uma boa performance em diversas atividades, das mais simples até algumas mais complexas. E esse modelo é na cor Azul. ",
+            "price": "R$ 1400.00",
+       } ,
+       3:{
+           "id": "114",
+            "img_src:": "phone.jpg,",
+            "nome": "Product 1",
+            "description": "Ta procurando um Smartphone com alto desempenho, capaz de executar as tarefas mais comuns do dia a dia com velocidade e eficiência? Então vem conhcer o Moto G54 da Motorola. Para começar, esse smartphone faz parte da linha Moto G, tradicional linha de celulares da Motorola. Ele possui armazenamento interno de 256GB, espaço suficiente para guardar os arquivos como fotos e vídeos. Ah, sem falar que ele conta com tecnologia 5G, Processador Dimensity 7020 e Memória RAM de 8GB, dessa forma você tem uma boa performance em diversas atividades, das mais simples até algumas mais complexas. E esse modelo é na cor Azul. ",
+            "price": "R$ 1500.00",
+       }
     }
+
+    cart: dict = {}
+
+    @staticmethod
+    def get_products() -> dict:
+        return Model.products
+    
+    @staticmethod
+    def get_cart() -> dict:
+        return Model.cart
+    
 class Product(ft.View):
-    def _ini__(self,) -> None:
+    def _ini__(self, page: ft.Page) -> None:
         super(Product, self).__init__(route="/products")
-        
+        self.page = page
+
+    def create_products(self, products: dict = Model.get_products()) -> None:
+
+        for _, values in products.items():
+            for key,value, in values.items():
+                if key == "img_src":
+                    ...
+                if key == "name":
+                    ...
+                if key == "description":
+                    ...
+                if key == "id":
+                    ...
+                if key == "price":
+                    ...
+    
+    def create_product_image(self, img_path: str):
+        return ft.Container(image_src=img_path,image_fit="fill",
+                            border_radius=6,padding=10)
+    
+    def create_product_text(self,name: str, description: str):
+        return ft.Column([ft.Text(name, size=18), ft.Text(description,size=11)])
+    
+    def create_product_event(self, price: str, idd: str):
+        return ft.Row(
+            [
+                ft.Text(price,size=14),
+                ft.IconButton("add",data=idd,on_click=self.add_to_cart)
+            ],
+            alignment="spaceBetween"
+        )
+
+    
 
 
 def main(page:ft.Page) -> None:
